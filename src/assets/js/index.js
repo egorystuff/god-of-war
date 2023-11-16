63bd9f6a0a4fe2fb5eddc7c6b6bfb07083ff3d6d
Yahor Yankavets <mr.yankovec@gmail.com>
2023-11-16 16:52:04 +0300

    added timer

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -1,3 +1,62 @@
 import "../styles/reset.scss";
 import "../styles/mixins.scss";
 import "../styles/styles.scss";
+
+const classes = {
+  opened: "opened",
+};
+
+// -----------------------------------------------------------------------------------------------------
+
+const menuLink = document.querySelectorAll(".menu-link");
+const header = document.querySelector("header");
+const menuButton = document.querySelector(".header-menu__button");
+
+// -----------------------------------------------------------------------------------------------------
+
+const toggleMenu = () => {
+  header.classList.toggle(classes.opened);
+};
+
+const scrollToSection = (e) => {
+  e.preventDefault();
+  const href = e.currentTarget.getAttribute("href");
+
+  if (!href && !href.startsWith("#")) return;
+
+  const section = href.slice(1);
+  const top = document.getElementById(section)?.offsetTop || 0;
+  window.scrollTo({ top, behavior: "smooth" });
+};
+
+// functions for counter operation
+const formatValue = (value) => (value < 10 ? `0${value}` : value);
+
+const getTimerValues = (diff) => {
+  return {
+    seconds: (diff / 1000) % 60,
+    minutes: (diff / (1000 * 60)) % 60,
+    hours: (diff / (1000 * 3600)) % 24,
+    days: (diff / (1000 * 3600 * 24)) % 30,
+  };
+};
+
+const setTimerValues = (values) => {
+  Object.entries(values).forEach(([key, value]) => {
+    const timerValue = document.getElementById(key);
+    timerValue.innerText = formatValue(Math.floor(value));
+  });
+};
+
+const startTimer = (date) => {
+  setInterval(() => {
+    const diff = new Date(date).getTime() - new Date().getTime();
+    setTimerValues(getTimerValues(diff));
+  }, 1000);
+};
+
+// -----------------------------------------------------------------------------------------------------
+
+startTimer("2023-11-30T00:00:00");
+menuButton.addEventListener("click", toggleMenu);
+menuLink.forEach((link) => link.addEventListener("click", scrollToSection));
